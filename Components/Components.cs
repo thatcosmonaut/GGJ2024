eb588dbd98b4654c94c8b035a7971af207570011
cass <prophet_goddess@protonmail.com>
2024-01-26 20:34:09 -0800

    1x1.png

--- Components/Components.cs
+++ Components/Components.cs
@@ -3,10 +3,4 @@ using MoonWorks.Math.Float;
 
 namespace GGJ2024.Components;
 
-public readonly record struct AABB(int W, int H);
-
 public readonly record struct Player(int Index);
-
-
-
-public readonly record struct Velocity(Vector2 Value);
