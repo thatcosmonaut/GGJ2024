177736e56247794c0ca52ca19654ef6012c98d21
cass <prophet_goddess@protonmail.com>
2024-01-26 20:01:53 -0800

    wip

--- Components/Components.cs
+++ Components/Components.cs
@@ -1,6 +1,12 @@
 using MoonWorks.Graphics;
+using MoonWorks.Math.Float;
 
 namespace GGJ2024.Components;
 
-public readonly record struct AABB(int X, int Y, int W, int H);
+public readonly record struct AABB(int W, int H);
+
 public readonly record struct Player(int Index);
+
+;public readonly record struct Position(int X, int Y)
+
+public readonly record struct Velocity(Vector2 Value);
---
+++ Systems/Motion.cs
@@ -0,0 +1,15 @@
+using System;
+using MoonTools.ECS;
+
+namespace GGJ2024.Systems;
+
+public class Motion : MoonTools.ECS.System
+{
+    public Motion(World world) : base(world)
+    {
+    }
+
+    public override void Update(TimeSpan delta)
+    {
+    }
+}
