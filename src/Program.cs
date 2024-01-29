d52fe1c1ee80b232ea6a3fd8ae95f34d2ae381dc
cosmonaut <evan@moonside.games>
2024-01-28 20:41:14 -0800

    add exception window and logging

--- src/Program.cs
+++ src/Program.cs
@@ -10,15 +10,24 @@ namespace GGJ2024
 {
 	class Program
 	{
+		public static string UserDataDirectory = $"{Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "ROLLANDCASH")}";
+
 		static void Main(string[] args)
 		{
+			if (!System.IO.Directory.Exists(UserDataDirectory))
+			{
+				System.IO.Directory.CreateDirectory(UserDataDirectory);
+			}
+
+			AppDomain.CurrentDomain.UnhandledException += HandleUnhandledException;
+
 			WindowCreateInfo windowCreateInfo = new WindowCreateInfo
 			{
 				WindowWidth = 1280,
 				WindowHeight = 720,
 				WindowTitle = "GGJ2024",
 				ScreenMode = ScreenMode.Fullscreen,
-				PresentMode = PresentMode.FIFORelaxed
+				PresentMode = PresentMode.Immediate
 			};
 
 			FrameLimiterSettings frameLimiterSettings = new FrameLimiterSettings
@@ -40,6 +49,21 @@ namespace GGJ2024
 			);
 
 			game.Run();
+		}
+
+		static void HandleUnhandledException(object sender, UnhandledExceptionEventArgs args)
+		{
+			Exception e = (Exception) args.ExceptionObject;
+			Logger.LogError("Unhandled exception caught!");
+			Logger.LogError(e.ToString());
+
+			Game.ShowRuntimeError("FLAGRANT SYSTEM ERROR", e.ToString());
+
+			StreamWriter streamWriter = new StreamWriter(Path.Combine(UserDataDirectory, "log.txt"));
+
+			streamWriter.WriteLine(e.ToString());
+			streamWriter.Flush();
+			streamWriter.Close();
 		}
 	}
 }
