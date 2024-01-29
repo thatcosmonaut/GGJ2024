2f55515b6dc0d64d72b80ffc644ebd7a4df629cf
cosmonaut <evan@moonside.games>
2024-01-28 18:12:15 -0800

    fix double item spawn

--- src/Systems/Product.cs
+++ src/Systems/Product.cs
@@ -120,7 +120,6 @@ public class Product : MoonTools.ECS.Manipulator
         var shelf = CreateEntity();
         Set(shelf, new Position(x, y));
         Set(shelf, new SpawnCategory(category));
-        Set(shelf, new CanSpawn(width, height));
 
 		for (var spawnerY = y; spawnerY < y + height * spawnStepDistance; spawnerY += spawnStepDistance)
 		{
@@ -140,7 +139,6 @@ public class Product : MoonTools.ECS.Manipulator
     {
         var shelf = CreateEntity();
         Set(shelf, new Position(x, y));
-        Set(shelf, new CanSpawn(width, height));
 
 		for (var spawnerY = y; spawnerY < y + height * spawnStepDistance; spawnerY += spawnStepDistance)
 		{
