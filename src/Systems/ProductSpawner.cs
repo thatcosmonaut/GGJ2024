aa5529e0765b37f8775ac604d4b44507b423b97d
Beau <teknopants@gmail.com>
2024-01-29 23:39:30 -0800

    shelf collision

--- src/Systems/ProductSpawner.cs
+++ src/Systems/ProductSpawner.cs
@@ -4,6 +4,7 @@ using GGJ2024.Components;
 using GGJ2024.Relations;
 using GGJ2024.Utility;
 using MoonTools.ECS;
+using MoonWorks.Math.Float;
 
 namespace GGJ2024.Systems;
 
@@ -40,13 +41,19 @@ public class ProductSpawner : MoonTools.ECS.Manipulator
         var topShelfY = 115;
         var bottomShelfY = 250;
         SpawnVerticalDoubleShelf(x, topShelfY, 0);
-        SpawnVerticalDoubleShelf(x, bottomShelfY, 2);
+        SpawnVerticalDoubleShelf(x, bottomShelfY - 8, 2);
         x = 640 - 80 - 80 - 10;
         SpawnVerticalDoubleShelf(x, topShelfY, 4);
         SpawnVerticalDoubleShelf(x, bottomShelfY, 6);
 
         Product.SpawnShelf(330 - spawnStepDistance - 16, 200, 4, 2);
+		var collision = CreateEntity();
+		Set(collision, new Solid());
+		Set(collision, new Position(330 - spawnStepDistance - 16, 200));
+		Set(collision, new Rectangle(-12, 8, 4 * spawnStepDistance, 8));
+
         Product.SpawnShelf(640 - spawnStepDistance + 10, 160, 1, 4, Categories[9]);
+
     }
 
     public void SpawnVerticalDoubleShelf(int x, int y, int categoryInt)
@@ -54,6 +61,11 @@ public class ProductSpawner : MoonTools.ECS.Manipulator
         var verticalProductAmount = 3;
         Product.SpawnShelf(x, y, 1, verticalProductAmount, Categories[categoryInt]);
         Product.SpawnShelf(x + spawnStepDistance, y, 1, verticalProductAmount, Categories[categoryInt + 1]);
+
+		var collision = CreateEntity();
+		Set(collision, new Solid());
+		Set(collision, new Position(x, y));
+		Set(collision, new Rectangle(12, -12, 8, (verticalProductAmount * spawnStepDistance) - 8));
     }
 
 	public void ClearProducts()
