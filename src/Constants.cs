729a0ecdc446ee39b2780eeeab68dfabc176937c
cass <prophet_goddess@protonmail.com>
2024-02-01 20:07:18 -0800

    bigger

--- src/Constants.cs
+++ src/Constants.cs
@@ -6,7 +6,7 @@ public static class Dimensions
 	public const int GAME_H = 360;
 
 	public const int SCORE_FONT_SIZE = 12;
-	public const int ORDER_FONT_SIZE = 8;
+	public const int ORDER_FONT_SIZE = 10;
 	public const int DIALOGUE_FONT_SIZE = 10;
 	public const int INSPECT_FONT_SIZE = 10;
 	public const int INGREDIENT_FONT_SIZE = 8;
