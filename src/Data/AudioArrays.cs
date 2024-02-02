251b0709bfde4650f465f461935b6527914e987c
Beau <teknopants@gmail.com>
2024-02-02 10:18:46 -0800

    cumulative money get sfx

---
+++ src/Data/AudioArrays.cs
@@ -0,0 +1,15 @@
+namespace RollAndCash.Content;
+
+public static class AudioArrays
+{
+    public static StaticSoundID[] Coins = {
+        StaticAudio.Coin1,
+        StaticAudio.Coin2,
+        StaticAudio.Coin3,
+        StaticAudio.Coin4,
+        StaticAudio.Coin5,
+        StaticAudio.Coin6,
+        StaticAudio.Coin7,
+        StaticAudio.Coin8
+    };
+}
