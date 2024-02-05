e67718b73333f83d2a528499d278dbfe32d961ee
cass <prophet_goddess@protonmail.com>
2024-02-04 20:38:10 -0800

    JERRY CREW

--- src/GameStates/CreditsState.cs
+++ src/GameStates/CreditsState.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using MoonWorks;
 using MoonWorks.Audio;
 using MoonWorks.Graphics;
@@ -21,6 +22,11 @@ public class CreditsState : GameState
     GraphicsPipeline TextPipeline;
     PersistentVoice Voice;
 
+    GraphicsPipeline HiResPipeline;
+
+    SpriteBatch HiResSpriteBatch;
+    Sampler LinearSampler;
+
     Queue<TextBatch> BatchPool = new Queue<TextBatch>();
     List<(TextBatch, Matrix4x4)> ActiveBatchTransforms = new List<(TextBatch, Matrix4x4)>();
 
@@ -40,7 +46,7 @@ public class CreditsState : GameState
         Game = game;
         TransitionState = transitionStateA;
 
-		var sound = StaticAudio.Lookup(StaticAudio.CreditsLaugh);
+        var sound = StaticAudio.Lookup(StaticAudio.CreditsLaugh);
         Voice = game.AudioDevice.Obtain<PersistentVoice>(sound.Format);
 
         GraphicsDevice = Game.GraphicsDevice;
@@ -66,14 +72,55 @@ public class CreditsState : GameState
             }
         );
 
+        var baseContentPath = Path.Combine(
+            System.AppContext.BaseDirectory,
+            "Content"
+        );
+
+        var shaderContentPath = Path.Combine(
+            baseContentPath,
+            "Shaders"
+        );
+
+        var vertShaderModule = new ShaderModule(GraphicsDevice, Path.Combine(shaderContentPath, "InstancedSpriteBatch.vert.refresh"));
+        var fragShaderModule = new ShaderModule(GraphicsDevice, Path.Combine(shaderContentPath, "InstancedSpriteBatch.frag.refresh"));
+
+
+        HiResPipeline = new GraphicsPipeline(
+            GraphicsDevice,
+            new GraphicsPipelineCreateInfo
+            {
+                AttachmentInfo = new GraphicsPipelineAttachmentInfo(
+                    new ColorAttachmentDescription(
+                        game.MainWindow.SwapchainFormat,
+                        ColorAttachmentBlendState.NonPremultiplied
+                    )
+                ),
+                DepthStencilState = DepthStencilState.Disable,
+                MultisampleState = MultisampleState.None,
+                PrimitiveType = PrimitiveType.TriangleList,
+                RasterizerState = RasterizerState.CCW_CullNone,
+                VertexInputState = new VertexInputState([
+                    VertexBindingAndAttributes.Create<PositionVertex>(0),
+                            VertexBindingAndAttributes.Create<SpriteInstanceData>(1, 1, VertexInputRate.Instance)
+                ]),
+                VertexShaderInfo = GraphicsShaderInfo.Create<ViewProjectionMatrices>(vertShaderModule, "main", 0),
+                FragmentShaderInfo = GraphicsShaderInfo.Create(fragShaderModule, "main", 1)
+            }
+        );
+
+        LinearSampler = new Sampler(GraphicsDevice, SamplerCreateInfo.LinearClamp);
+        HiResSpriteBatch = new SpriteBatch(GraphicsDevice);
+
         Rando.Shuffle(Names);
     }
 
     public override void Start()
     {
-		CreditsTime = 0;
+        Rando.Shuffle(Names);
+        CreditsTime = 0;
 
-		var sound = StaticAudio.Lookup(StaticAudio.CreditsLaugh);
+        var sound = StaticAudio.Lookup(StaticAudio.CreditsLaugh);
         Voice.Submit(sound);
         Voice.Play();
     }
@@ -107,10 +154,26 @@ public class CreditsState : GameState
             }
             ActiveBatchTransforms.Clear();
 
-            AddString("JERRY CREW", 100, Color.White, new Position(960, 180));
-            AddString("is", 50, Color.White, new Position(960, 300));
 
-            var y = 420;
+            HiResSpriteBatch.Reset();
+
+            var logoAnimation = SpriteAnimations.Logo_JerryCrew;
+            var sprite = logoAnimation.Frames[0];
+            var logoPosition = new Position(665, 80);
+
+            HiResSpriteBatch.Add(
+                new Vector3(logoPosition.X, logoPosition.Y, -1f),
+                0,
+                new Vector2(sprite.SliceRect.W, sprite.SliceRect.H),
+                Color.White,
+                sprite.UV.LeftTop, sprite.UV.Dimensions
+            );
+
+            HiResSpriteBatch.Upload(commandBuffer);
+
+            AddString("is", 50, Color.White, new Position(960, 350));
+
+            var y = 470;
             foreach (var name in Names)
             {
                 AddString(name, 70, Color.White, new Position(960, y));
@@ -124,6 +187,16 @@ public class CreditsState : GameState
 
             commandBuffer.BeginRenderPass(
                 new ColorAttachmentInfo(swapchainTexture, Color.Black)
+            );
+
+            var hiResViewProjectionMatrices = new ViewProjectionMatrices(Matrix4x4.Identity, GetHiResProjectionMatrix());
+
+            HiResSpriteBatch.Render(
+                commandBuffer,
+                HiResPipeline,
+                TextureAtlases.TP_HiRes.Texture,
+                LinearSampler,
+                hiResViewProjectionMatrices
             );
 
             if (ActiveBatchTransforms.Count > 0)
